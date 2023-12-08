4866d93705e0dd01481a946b357bea83f49ab503
Chikage Haruse <contact@chikage.net>
2023-12-08 23:14:17 +0900

    アップデートの有無がわかりやすいように修正

--- build.sh
+++ build.sh
@@ -6,8 +6,18 @@ APT_UPGRADABLE_LIST=`docker run --rm --entrypoint "bash" chikage8640/warp-docker
 docker pull ubuntu:22.04
 UBUNTU_CREATED=`docker inspect -f '{{ .Created }}' ubuntu:22.04` 
 
+if [[ $APT_UPGRADABLE_LIST == *cloudflare-warp* ]]; then
+  echo "cloudflare-warp is upgradable"
+fi
+
+if [[ $UBUNTU_CREATED == $(date +%Y-%m-%d --date '1 day ago')* ]]; then
+  echo "Ubuntu is upgradable"
+fi
+
 if [[ $APT_UPGRADABLE_LIST == *cloudflare-warp* ]] || [[ $UBUNTU_CREATED == $(date +%Y-%m-%d --date '1 day ago')* ]]; then
   # ビルド処理
   docker build --cache-from $USERNAME/warp-docker:latest --build-arg CHASHEBUST=$(date +%s) -t $USERNAME/warp-docker:latest -t $USERNAME/warp-docker:$(date +%Y-%m-%d) .
   docker push -a $USERNAME/warp-docker
+else
+  echo "No updates available"
 fi
