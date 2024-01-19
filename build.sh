77f9377539d5893e2d46042d0f1b16d2ab60d5cf
Chikage Haruse <contact@chikage.net>
2024-01-20 02:58:49 +0900

    エラー修正

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ docker pull ubuntu:22.04
 UBUNTU_HASH=`docker inspect -f '{{ .RootFS.Layers }}' ubuntu:22.04 | tr -d '[]' | awk -F' ' '{print $1}'`
 CF_WARP_HASH=`docker inspect -f '{{ .RootFS.Layers }}' $USERNAME/cf-warp:latest | tr -d '[]' | awk -F' ' '{print $1}'`
 GOST_LATEST=`curl https://api.github.com/repos/ginuerzh/gost/releases/latest | grep -o '"tag_name": "[^"]*"' | sed -n 's/.*"tag_name": "v\([^"]*\)".*/\1/p'`
-GOST_VERSION=`docker run --rm --entrypoint "bash" chikage8640/cf-warp:latest -c "gost -V  | awk -F' ' '{print \$2}'"`
+GOST_VERSION=`docker run --rm --entrypoint "bash" chikage8640/cf-warp:latest -c "gost -V" | awk -F' ' '{print $2}'`
 APT_UPGRADABLE_LIST=`docker run --rm --entrypoint "bash" chikage8640/cf-warp:latest -c "apt-get update &> /dev/null && apt list -oApt::Cmd::Disable-Script-Warning=1 --upgradable"`
 echo "Ubuntu hash: $UBUNTU_HASH"
 echo "cf-warp hash: $CF_WARP_HASH"
@@ -30,8 +30,7 @@ fi
 
 if [[ $APT_UPGRADABLE_LIST == *cloudflare-warp* ]] || [ $GOST_LATEST != $GOST_VERSION ] || [ $UBUNTU_HASH != $CF_WARP_HASH ]; then
   # ビルド処理
-  docker build --cache-from $USERNAME/cf-warp:latest --platform linux/amd64,linux/arm64 --build-arg CHASHEBUST=$(date +%s),GOST_VERSION=$GOST_LATEST -t $USERNAME/cf-warp:latest -t $USERNAME/cf-warp:$(date +%Y-%m-%d) .
-  docker push -a $USERNAME/cf-warp
+  docker buildx build --cache-from $USERNAME/cf-warp:latest --push --platform linux/amd64,linux/arm64 --build-arg CHASHEBUST=$(date +%s) --build-arg GOST_VERSION=$GOST_LATEST -t $USERNAME/cf-warp:latest -t $USERNAME/cf-warp:$(date +%Y-%m-%d) .
 else
   echo "No updates available"
 fi
